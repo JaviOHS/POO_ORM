790d4a0501d6984d4abd92a101ac07ba0b7909e4
JaviOHS <jharos@unemi.edu.ec>
2024-06-16 01:15:36 -0500

    Avances en Home

--- orm_project/sga/urls.py
+++ orm_project/sga/urls.py
@@ -1,8 +1,15 @@
 from django.urls import include, path
-from sga import views
+from sga import views 
  
 app_name='sga'
 
 urlpatterns = [    
     path('', views.home, name='home'),
+    
+    path('periods/', views.period_list, name='period_list'),
+    path('teachers/', views.teacher_list, name='teacher_list'),
+    path('students/', views.student_list, name='student_list'),
+    path('subjects/', views.subject_list, name='subject_list'),
+    path('notes/', views.notes_list, name='notes_list'),
+    path('notes_detail/', views.notes_detail_list, name='notes_detail_list'),
 ]
--- orm_project/sga/views.py
+++ orm_project/sga/views.py
@@ -6,3 +6,32 @@ def home(request):
             "title2": "Sistema de Gestion Academica"}
     return render(request, 'sga/home.html', data)
 
+def period_list(request):
+    data = {"title1": "SGA - Periodos",
+            "title2": "Listado de Periodos"}
+    return render(request, 'sga/periods.html', data)
+
+def teacher_list(request):
+    data = {"title1": "SGA - Docentes",
+            "title2": "Listado de Docentes"}
+    return render(request, 'sga/teachers.html', data)
+
+def student_list(request):
+    data = {"title1": "SGA - Estudiantes",
+            "title2": "Listado de Estudiantes"}
+    return render(request, 'sga/students.html', data)
+
+def subject_list(request):
+    data = {"title1": "SGA - Materias",
+            "title2": "Listado de Materias"}
+    return render(request, 'sga/subjects.html', data)
+
+def notes_list(request):
+    data = {"title1": "SGA - Notas",
+            "title2": "Listado de Notas"}
+    return render(request, 'sga/notes.html', data)
+
+def notes_detail_list(request):
+    data = {"title1": "SGA - Notas Detalle",
+            "title2": "Listado de Notas Detalle"}
+    return render(request, 'sga/notes_detail.html', data)
